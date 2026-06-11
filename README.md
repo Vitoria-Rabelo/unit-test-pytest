838ce6ed6b4670907731853ab668dd60af2abe65
Vitória Rabelo <104806771+Vitoria-Rabelo@users.noreply.github.com>
2026-06-11 17:04:47 -0300

    ft: my first unit test

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # unit-test-pytest
 Practices from the Udemy course "Python Unit Testing Fundamentals" using unittest and pytest.
+
+You can run the tests with the bellow command:
+python -m unittest calculator.py
---
+++ calculator.py
@@ -0,0 +1,14 @@
+def calc_add(num1, num2):
+    return num1 + num2
+
+def calc_subt(num1, num2):
+    return num1 - num2
+
+def calc_mult(num1, num2):
+    return num1 * num2
+
+def calc_div(num1, num2):
+    return num1 / num2 
+
+if __name__ == '__main__':
+    print("Import and use me!")
