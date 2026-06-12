f0a9b91fb98c42c8ee1851c8157c61c52e20a036
Vitória Rabelo <104806771+Vitoria-Rabelo@users.noreply.github.com>
2026-06-12 16:37:38 -0300

    ft: add dotstrings

--- test_calculator_v2.py
+++ test_calculator_v2.py
@@ -2,12 +2,21 @@ import unittest
 from calculator_v2 import Calculator
 
 class TestCalculatorV2(unittest.TestCase):
+    """
+    Tests for the base functionality of the calculator class
+    """
     def test_add_funcionality(self):
+        """
+        Test case to test add functionality with two positive numbers
+        """
         calc1 = Calculator(2, 3)
         result = calc1.calc_add()
         self.assertEqual(result, 5)
 
     def test_add_functionality_with_one_negative_number(self):
+        """
+        Test case to test add functionality with two negative numbers
+        """
         calc1 = Calculator(10, -30)
         result = calc1.calc_add()
         self.assertEqual(result, -20)
